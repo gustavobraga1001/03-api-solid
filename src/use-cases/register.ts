8ac573276b2aa8538ca76d37e4047cf0d71ed7b3
gustavobraga1001 <100322388+gustavobraga1001@users.noreply.github.com>
2024-11-08 13:08:39 -0300

    increments repository patterns

---
+++ src/use-cases/register.ts
@@ -0,0 +1,28 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { hash } from 'bcryptjs'
+
+interface RegitserUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export class RegitserUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({ name, email, password }: RegitserUseCaseRequest) {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new Error('E-mail already exists.')
+    }
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+  }
+}
